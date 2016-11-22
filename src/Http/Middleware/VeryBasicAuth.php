32a11dc87aa27bd3fbd6b220f32205dc2ab12022
Marcus Olsson <hello@marcusolsson.me>
2016-11-22 13:04:43 +0100

    Updated to handle views as well.

--- src/Http/Middleware/VeryBasicAuth.php
+++ src/Http/Middleware/VeryBasicAuth.php
@@ -18,12 +18,22 @@ class VeryBasicAuth
         $veryBasicAuthPass = config('very_basic_auth.password');
         $veryBasicAuthEnvs = config('very_basic_auth.envs');
         $veryBasicAuthMsg  = config('very_basic_auth.error_message');
+        $veryBasicAuthView = config('very_basic_auth.error_view');
 
         // Check if middleware is in use in current environment
         if(in_array(app()->environment(), $veryBasicAuthEnvs)) {
             if($request->getUser() != $veryBasicAuthUser || $request->getPassword() != $veryBasicAuthPass) {
-                $headers = array('WWW-Authenticate' => 'Basic');
-                return response($veryBasicAuthMsg, 401, $headers);
+
+                $header = ['WWW-Authenticate' => 'Basic'];
+
+                // If view is available
+                if ($veryBasicAuthView) {
+                    return response()->view($veryBasicAuthView, [], 401)
+                        ->withHeaders($header);
+                }
+
+                // Else return default message
+                return response($veryBasicAuthMsg, 401, $header);
             }
         }
 
